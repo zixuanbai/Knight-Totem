f3565d4ef2e4216b3692b23b726b9afabb74788b
zixuanbai <69137132+zixuanbai@users.noreply.github.com>
2023-11-30 01:31:41 +1100

    finish player left right movement

---
+++ Knight-Totem/Assets/Script/Player/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerInputControl inputControl;
+    private Rigidbody2D rb;
+    public Vector2 inputDirection;
+    public float speed;
+
+
+   private void Awake()
+    {
+        inputControl = new PlayerInputControl();
+        rb=GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        inputControl.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputControl.Disable();
+    }
+
+    private void Update()
+    {
+        inputDirection = inputControl.Gameplay.Move.ReadValue<Vector2>();
+        transform.Translate(inputDirection * speed * Time.deltaTime);
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        rb.velocity = new Vector2(inputDirection.x * speed * Time.deltaTime, rb.velocity.y);
+        if (inputDirection.x > 0 && transform.localScale.x < 0 ||
+            inputDirection.x < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
