956cb3f659ae43de777b5dbac27cb1edbba8e0ad
zixuanbai <69137132+zixuanbai@users.noreply.github.com>
2023-12-11 01:57:22 +1100

    add enemy Patrol

--- Knight-Totem/Assets/Script/Enemy/Enemy.cs
+++ Knight-Totem/Assets/Script/Enemy/Enemy.cs
@@ -5,23 +5,34 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private PhysicsCheck physicsCheck;
     protected Animator anim;
     
     public float nomalSpeed;
     public float chaseSpeed;
     public float currentSpeed;
     public Vector3 faceDir;
-
+    public float waitTime;
+    public float waitTimeCounter;
+    public bool wait;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         currentSpeed = nomalSpeed;
+        physicsCheck = GetComponent<PhysicsCheck>();
+        waitTimeCounter = waitTime;
     }
     
     private void Update()
     {
         faceDir = new Vector3(-transform.localScale.x, 0, 0);
+        if((physicsCheck.touchLeftWall&&faceDir.x<0) || (physicsCheck.touchRightWall&&faceDir.x>0))
+        {
+            wait = true;
+            anim.SetBool("Walk", false);
+        }
+        TimeCounter();
     }
     private void FixedUpdate()
     {
@@ -32,4 +43,20 @@ public class Enemy : MonoBehaviour
     {
         rb.velocity = new Vector2(currentSpeed * faceDir.x * Time.deltaTime, rb.velocity.y);
     }
+
+    public void TimeCounter() 
+    {
+        if (wait)
+        {
+            waitTimeCounter -= Time.deltaTime;
+            if (waitTimeCounter <= 0) 
+            {
+                wait = false;
+                waitTimeCounter = waitTime;
+                transform.localScale = new Vector3(faceDir.x, 1, 1);
+            }
+        
+        }
+    }
+
 }
--- Knight-Totem/Assets/Script/General/PhysicsCheck.cs
+++ Knight-Totem/Assets/Script/General/PhysicsCheck.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class PhysicsCheck : MonoBehaviour
 {
+    private CapsuleCollider2D coll;
     public LayerMask groundLayer;
+    public bool manual;
     public bool isGround;
     public float checkRaduis;
     public Vector2 bottomOffset;
+    public Vector2 leftOffset;
+    public Vector2 rightOffset;
+    public bool touchLeftWall;
+    public bool touchRightWall;
 
+    private void Awake()
+    {
+        coll = GetComponent<CapsuleCollider2D>();
+        if (manual)
+        {
+            rightOffset = new Vector2((coll.bounds.size.x + coll.offset.x) / 2, coll.bounds.size.y / 2);
+            leftOffset= new Vector2(-rightOffset.x,rightOffset.y);
+        }
+    }
 
     private void Update()
     {
@@ -17,11 +32,15 @@ public class PhysicsCheck : MonoBehaviour
 
    public void Check()
     {
-      isGround= Physics2D.OverlapCircle((Vector2)transform.position, checkRaduis, groundLayer);
+      isGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, checkRaduis, groundLayer);
+      touchLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, checkRaduis, groundLayer);
+      touchRightWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, checkRaduis, groundLayer);
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.DrawWireSphere((Vector2)transform.position + bottomOffset, checkRaduis);
+        Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset, checkRaduis);
+        Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset, checkRaduis);
     }
 }
