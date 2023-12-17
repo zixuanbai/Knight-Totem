e328d2a0349a0a0b6d1eeb74ad27dfe1b335c5c9
zixuanbai <69137132+zixuanbai@users.noreply.github.com>
2023-12-18 01:42:26 +1100

    finish camera and shake

---
+++ Knight-Totem/Assets/Script/CameraControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraControl : MonoBehaviour
+{
+    public CinemachineImpulseSource impulseSource;
+    public VoidEventSO cameraShakeEvent;
+
+    private void OnEnable()
+    {
+        cameraShakeEvent.OnEventRaised += OnCameraShakeEvent; 
+    }
+
+    private void OnDisable()
+    {
+        cameraShakeEvent.OnEventRaised -= OnCameraShakeEvent; 
+    }
+
+    private void OnCameraShakeEvent()
+    {
+        impulseSource.GenerateImpulse();
+    }
+}
