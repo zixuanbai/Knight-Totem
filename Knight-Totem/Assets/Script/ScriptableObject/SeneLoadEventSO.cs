4ba9f02ff357bc1b8dbc70979a84d7489ad80dd9
zixuanbai <69137132+zixuanbai@users.noreply.github.com>
2023-12-20 04:19:54 +1100

    finish menu and level build

---
+++ Knight-Totem/Assets/Script/ScriptableObject/SeneLoadEventSO.cs
@@ -0,0 +1,11 @@
+using UnityEngine.AddressableAssets;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game Scene/GameSceneSO")]
+
+public class GameScenceSO : ScriptableObject {
+
+    public AssetReference sceneReference;
+
+}
+
---
+++ Knight-Totem/Assets/Script/UI/SenceManagement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SenceManagement : MonoBehaviour
+{
+    public Button startButton;
+    public Button exitButton;
+
+    void Start()
+    {
+        
+        startButton.onClick.AddListener(StartGame);
+        exitButton.onClick.AddListener(ExitGame);
+    }
+
+    void StartGame()
+    {
+        
+        SceneManager.LoadScene("Level1");
+    }
+
+    void ExitGame()
+    {
+        
+        Application.Quit();
+    }
+}
